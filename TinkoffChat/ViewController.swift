832222ea2436417dbf58524d9314600325086fe1
Vasily Ponomarev <vaspondm@gmail.com>
2017-03-18 17:55:29 +0300

    main changes for second HW

--- TinkoffChat/ViewController.swift
+++ TinkoffChat/ViewController.swift
@@ -10,15 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textColorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func saveButtonAction(_ sender: UIButton) {
+        print("This scene should be saved")
+    }
+    @IBAction func changeColorButton(_ sender: UIButton) {
+        textColorLabel.textColor = sender.backgroundColor
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
 
 }
