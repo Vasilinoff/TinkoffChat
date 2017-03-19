92c306c98d10542a9ceb05be40f4c2f554cafc39
Vasily Ponomarev <vaspondm@gmail.com>
2017-03-19 14:42:03 +0300

    add descriprionPrint function

--- TinkoffChat/ViewController.swift
+++ TinkoffChat/ViewController.swift
@@ -8,18 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     
+    @IBOutlet weak var aboutTextView: UITextView!
     @IBOutlet weak var userImageView: UIImageView!
     @IBOutlet weak var textColorLabel: UILabel!
+    @IBOutlet weak var userNameTextField: UITextField!
     
     let pickerController = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.userNameTextField.delegate = self
+        
         print(#function)
+        
+        descriptionPrint()
         
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
         
@@ -27,6 +33,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        userNameTextField.resignFirstResponder()
+        return (true)
+    }
     
     @IBAction func saveButtonAction(_ sender: UIButton) {
         print("Сохранение данных профиля")
@@ -35,6 +45,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBAction func changeColorButton(_ sender: UIButton) {
         textColorLabel.textColor = sender.backgroundColor
     }
+    
+    
     
     @IBAction func choosePicture(_ sender: Any) {
         pickerController.delegate = self
@@ -81,6 +93,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             self.userImageView.image = pickedImage
         }
     }
+    
+    func descriptionPrint() {
+        let outlets = [aboutTextView, userImageView, userNameTextField, textColorLabel] as [UIView]
+        
+        print("\n")
+        
+        for control in outlets {
+            print(control.description)
+            print("\n")
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -93,24 +116,32 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.viewWillAppear(true)
         
         print(#function)
+        
+        descriptionPrint()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
         
         print(#function)
+        
+        descriptionPrint()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(true)
         
         print(#function)
+        
+        descriptionPrint()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(true)
         
         print(#function)
+        
+        descriptionPrint()
     }
 
 
