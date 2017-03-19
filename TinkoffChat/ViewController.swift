e3fa43e369faf648d39ef3ff3488e1c003b83264
Vasily Ponomarev <vaspondm@gmail.com>
2017-03-19 13:55:22 +0300

    Fixed everything, that was asked

--- TinkoffChat/ViewController.swift
+++ TinkoffChat/ViewController.swift
@@ -19,41 +19,47 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print(#function)
+        
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
         
         view.addGestureRecognizer(tap)
         
     }
+    
+    
     @IBAction func saveButtonAction(_ sender: UIButton) {
         print("Сохранение данных профиля")
     }
+    
     @IBAction func changeColorButton(_ sender: UIButton) {
         textColorLabel.textColor = sender.backgroundColor
     }
     
     @IBAction func choosePicture(_ sender: Any) {
-        
-        
-        
         pickerController.delegate = self
         pickerController.allowsEditing = false
         
-        let alertController = UIAlertController(title: "Add a Picture", message: "Choose From", preferredStyle: .actionSheet)
-        let cameraAction = UIAlertAction(title: "Camera", style: .default) { (action) in
+        let alertController = UIAlertController(title: "Добавить Изображение", message: "Выбрать из", preferredStyle: .actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "Камера", style: .default) { (action) in
             self.pickerController.sourceType = .camera
             self.present(self.pickerController, animated: true, completion: nil)
         }
-        let photosLibraryAction = UIAlertAction(title: "Photos Library", style: .default) { (action) in
+        
+        let photosLibraryAction = UIAlertAction(title: "Библиотека Фото", style: .default) { (action) in
             self.pickerController.sourceType = .photoLibrary
             self.present(self.pickerController, animated: true, completion: nil)
         }
-        let savedPhotosAction = UIAlertAction(title: "Saved Photos Album", style: .default) { (action) in
+        
+        let savedPhotosAction = UIAlertAction(title: "Сохраненные Фото", style: .default) { (action) in
             self.pickerController.sourceType = .savedPhotosAlbum
             self.present(self.pickerController, animated: true, completion: nil)
         }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         
-        let removeImageAction = UIAlertAction(title: "remove", style: .default) { (action) in
+        let cancelAction = UIAlertAction(title: "Отменить", style: .cancel, handler: nil)
+        
+        let removeImageAction = UIAlertAction(title: "Удалить фото", style: .destructive) { (action) in
             self.userImageView.image = UIImage(named: "placeholder")
             
         }
@@ -65,14 +71,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         alertController.addAction(removeImageAction)
         
         present(alertController, animated: true, completion: nil)
-
-        
-        
-        
     }
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info:[String: AnyObject]) {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info:[String: Any]) {
+        
         self.dismiss(animated: true, completion: nil)
+        
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             self.userImageView.image = pickedImage
         }
@@ -80,12 +84,33 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func dismissKeyboard() {
-        //Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        print(#function)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        print(#function)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        
+        print(#function)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(true)
+        
+        print(#function)
     }
 
 
